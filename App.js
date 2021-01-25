d4231b4d24822514280239f6c1ca945408a12be0
T3sultan <incognito.soul011@gmail.com>
2021-01-26 02:14:46 +0600

    Animated FlatList done

--- App.js
+++ App.js
@@ -1,114 +1,140 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import {StyleSheet,View,Text,ActivityIndicator,Image,Animated,FlatList} from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const BIG_IMG = 'https://cdn.pixabay.com/photo/2020/01/31/07/53/man-4807395_960_720.jpg';
+const ITEM_MARGIN_BOTTOM = 20;
+const ITEM_PADDING = 10;
+const HEIGHT_IMG = 100;
+const ITEM_SIZE = HEIGHT_IMG + ITEM_PADDING * 2 + ITEM_MARGIN_BOTTOM;
 
-const App: () => React$Node = () => {
+const App = () => {
+  const scrollY = React.useRef(new Animated.Value(0)).current;
+
+  const [data, setdata] = useState([]);
+  const [isLoading, setisLoading] = useState(true);
+
+  useEffect(() => {
+    getListPhotos();
+    return () => {
+
+    }
+  }, [])
+
+  getListPhotos = () => {
+    const apiURL = 'https://jsonplaceholder.typicode.com/photos';
+    fetch(apiURL)
+      .then((res) => res.json())
+      .then((resJson) => {
+        setdata(resJson)
+      }).catch((error) => {
+        console.log("Request Api Error", error);
+      }).finally(() => setisLoading(false));
+
+  }
+
+  renderItem = ({ item, index }) => {
+    const scale = scrollY.interpolate({
+      inputRange: [
+        -1, 0,
+        ITEM_SIZE * index,
+        ITEM_SIZE * (index + 2)
+      ],
+      outputRange: [1, 1, 1, 0]
+    })
+    const opacity = scrollY.interpolate({
+      inputRange: [
+        -1, 0,
+        ITEM_SIZE * index,
+        ITEM_SIZE * (index + .6)
+      ],
+      outputRange: [1, 1, 1, 0]
+    })
+    return (
+      <Animated.View style={[
+        styles.item,
+        {
+          transform: [{scale}],
+          opacity
+        }
+      ]}>
+        <Image
+          style={styles.image}
+          source={{ uri: item.url }}
+          resizeMode="contain"
+
+        />
+        <View style={styles.wrapText}>
+          <Text style={styles.fontSize}>{index + '. ' + item.title}</Text>
+
+        </View>
+
+      </Animated.View>
+    )
+
+  }
+
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
+    <View style={styles.container}>
+      <Image
+        source={{ uri: BIG_IMG }}
+        style={StyleSheet.absoluteFillObject}
+        blurRadius={70}
+      />
+      {isLoading ? <ActivityIndicator /> : (
+        <Animated.FlatList
+          data={data}
+          keyExtractor={item => `key-${item.id}`}
+          renderItem={renderItem}
+          contentContainerStyle={{
+            padding: 20
+          }}
+          onScroll={Animated.event(
+            [{ nativeEvent: { contentOffSet: {y: scrollY } } }],
+            { useNativeDriver: true }
+
           )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+        />
+
+      )}
+
+    </View>
+
   );
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+  container: {
+    flex: 1,
 
+  },
+  fontSize: {
+    fontSize: 18,
+
+  },
+  image: {
+    width: 100,
+    height: HEIGHT_IMG,
+  },
+  wrapText: {
+    flex: 1,
+    marginLeft: 10,
+    justifyContent: "center"
+  },
+  item: {
+    flexDirection: 'row',
+    marginBottom: ITEM_MARGIN_BOTTOM,
+    borderRadius: 10,
+    backgroundColor: "#fff",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 10
+    },
+    shadowOpacity: .3,
+    shadowRadius: 20,
+    padding: ITEM_PADDING,
+  }
+
+});
 export default App;
